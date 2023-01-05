6518aaab3ddddc51d3b33c5e5067746f840975dd
Ashwini <salunkheashwini1998@gmail.com>
2023-01-05 20:49:53 +0530

    this is folder

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LineComparisonStatic
+{
+    public class Method
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("***** Welcome to the Line Comparison computation Program for Static Methods.*****");
+
+            LineComparison.checkLine();
+
+        }
+    }
+}
